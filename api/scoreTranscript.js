de1523dee207ccd29463efe93cad93c6c3d4889c
ndgsmiletarp-ctrl <ndg.smile.tarp@gmail.com>
2025-11-23 23:32:18 +0000

    777

--- api/scoreTranscript.js
+++ api/scoreTranscript.js
@@ -7,6 +7,20 @@ const sentiment = require("../scoring/sentiment");
 const feedbackGen = require("../scoring/feedback");
 
 module.exports = async (req, res) => {
+
+  // -------------------------------
+  // CORS FIX (preflight + headers)
+  // -------------------------------
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Preflight response
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  // Only POST allowed
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
